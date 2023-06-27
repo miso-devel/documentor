1b53ef8d3bceea696811ff378b852f7138e83b1c
miso-devel <kengo279@icloud.com>
2023-06-27 23:54:38 +0900

    draft init func

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log(Deno.args);
+import { createConstructure } from "./utility/init.ts";
+
+const cwd = Deno.cwd();
+type TScriptKind = "init" | "update";
+const scriptKind = Deno.args[0] as TScriptKind;
+if (scriptKind === "init") {
+  const result = await createConstructure({
+    name: cwd.split("/")[cwd.split("/").length - 1],
+    entry: ".",
+    isDir: true,
+  });
+
+  await Deno.writeTextFile("documentor.json", JSON.stringify(result));
+}
---
+++ src/utility/init.ts
@@ -0,0 +1,48 @@
+type TCreatePathProps = { prePath: string; name: string };
+const createPath = ({ prePath, name }: TCreatePathProps) => {
+  return prePath + "/" + name;
+};
+
+type TDirectoryInfo = {
+  name: string;
+  description: string;
+  directories: TDirectoryInfo[];
+};
+
+const ignoreList = [".git"];
+
+type TCreateConstructureProps = {
+  name: string;
+  entry: string;
+  isDir: boolean;
+};
+export const createConstructure = async (
+  { name, entry, isDir }: TCreateConstructureProps,
+) => {
+  const directoryInfo: TDirectoryInfo = {
+    name,
+    description: "",
+    directories: [],
+  };
+
+  if (!isDir) return { name: entry, description: "", directories: [] };
+
+  for await (const dir of Deno.readDir(entry)) {
+    const path = createPath({ prePath: entry, name: dir.name });
+
+    if (dir.isFile) {
+      await createConstructure({ name: dir.name, entry: path, isDir: false });
+    } else {
+      const structure = await createConstructure({
+        name: dir.name,
+        entry: path,
+        isDir: true,
+      });
+      if (!ignoreList.includes(dir.name)) {
+        directoryInfo.directories.push(structure);
+      }
+    }
+  }
+
+  return directoryInfo;
+};
