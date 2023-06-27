cddcb8b8f897594050cb50103e5173f453815746
miso-devel <kengo279@icloud.com>
2023-06-28 01:04:39 +0900

    if to switch

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,22 @@
-import { createConstructure } from "./utility/init.ts";
+import { createConstructure } from './utility/init.ts';
 
-const cwd = Deno.cwd();
-type TScriptKind = "init" | "update";
+const cwd = Deno.cwd().split('/')[Deno.cwd().split('/').length - 1];
+type TScriptKind = 'init' | 'update';
 const scriptKind = Deno.args[0] as TScriptKind;
-if (scriptKind === "init") {
-  const result = await createConstructure({
-    name: cwd.split("/")[cwd.split("/").length - 1],
-    entry: ".",
-    isDir: true,
-  });
 
-  await Deno.writeTextFile("documentor.json", JSON.stringify(result));
+switch (scriptKind) {
+  case 'init':
+    {
+      const result = await createConstructure({ name: cwd, entry: '.', isDir: true });
+      await Deno.writeTextFile('documentor.json', JSON.stringify(result));
+    }
+    break;
+
+  case 'update':
+    console.log('update');
+    break;
+
+  default:
+    console.log('please write args');
+    break;
 }
