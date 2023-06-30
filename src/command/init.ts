5b24ad6387c797a5f73554647e8ec1c524ef7e0e
miso-devel <kengo279@icloud.com>
2023-06-30 14:14:12 +0900

    remove simple func

--- src/command/init.ts
+++ src/command/init.ts
@@ -1,18 +1,16 @@
-type TCreatePathProps = { prePath: string; name: string };
-type TDirectoryInfo = { name: string; info: string; dir: TDirectoryInfo[] };
-type TCreateConstructureProps = { name: string; entry: string; isDir: boolean };
+import { TDirectory } from '../types/TDirectory.d.ts';
 
-const createPath = ({ prePath, name }: TCreatePathProps) => prePath + '/' + name;
+type TCreateConstructureProps = { name: string; entry: string; isDir: boolean };
 
 const ignoreList = ['.git'];
 
 export const createConstructure = async ({ name, entry, isDir }: TCreateConstructureProps) => {
-  const directoryInfo: TDirectoryInfo = { name, info: '', dir: [] };
+  const directoryInfo: TDirectory = { name, info: '', dir: [] };
 
   if (!isDir) return { name: entry, info: '', dir: [] };
 
   for await (const dir of Deno.readDir(entry)) {
-    const path = createPath({ prePath: entry, name: dir.name });
+    const path = entry + '/' + name;
 
     if (dir.isFile) {
       await createConstructure({ name: dir.name, entry: path, isDir: false });
